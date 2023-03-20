e807a394805a679ff8643beb39b58556c08d5b4b
WieteskaKarolina <wieteskakarolina02@gmail.com>
2023-03-20 14:21:35 +0100

    fix server.js from lab1

--- lab2/source/server.js
+++ lab2/source/server.js
@@ -2,8 +2,27 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.get('/', function(req, res) {
+    const videoFile = req.query.videoFile;
+    const audioFile = req.query.audioFile;
+  
+    if (!videoFile && !audioFile) {
+      res.send('Please provide a video or audio file.');
+    } 
+    else {
+      if(videoFile){
+        res.send(`<video id="videoPlayer" controls>
+                  <source src="${videoFile}" type="video/mp4">
+                  Your browser does not support the video tag.
+                  </video>`);
+      } 
+      else{
+        res.send(`<audio id="audioPlayer" controls>
+                  <source src="${audioFile}" type="audio/mpeg">
+                  Your browser does not support the audio tag.
+                  </audio>`);
+      }
+    }
+  });
 
 app.listen(4080)
