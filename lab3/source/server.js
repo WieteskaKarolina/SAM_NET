b8228afe0af4e1c736469c0c875daf7cf01473a1
WieteskaKarolina <wieteskakarolina02@gmail.com>
2023-04-03 14:31:36 +0200

    add style

--- lab3/source/server.js
+++ lab3/source/server.js
@@ -29,7 +29,8 @@ app.get('/', function(req, res) {
         res.write("source src='" + videoFile +"' />");
         res.write("</video>");
         //button
-        res.write("<script>function foo(){ document.getElementById('videoPlayer').src = 'cancel.mp4';}</script>");
+        res.write("<br/>");
+        res.write("<script>function foo1(){ document.getElementById('videoPlayer').src = 'cancel.mp4';}</script>");
         res.write("<button id='videoCancel' onclick='foo1()'>Click me</button>");
         
     } 
@@ -38,12 +39,25 @@ app.get('/', function(req, res) {
         res.write("<source src='" + audioFile+"' />");
         res.write("</audio>");
         //button
+        res.write("<br/>");
         res.write("<script>function foo(){ document.getElementById('audioPlayer').src = 'cancel.mp3';}</script>");
         res.write("<button id='audioCancel' onclick='foo()'>Click me</button>");
     }
     if(posterImage){
         res.write("<img src='" + posterImage +"' id='posterImage'>");
     }
+
+    res.write("<style>");
+    res.write("table, th, td {");
+    res.write("  border:1px solid black;}");
+    res.write("</style>");
+    res.write("<table id='playlist_table'>");
+    res.write("<tr><th>No.</th>");
+    res.write("<th>URL</th>");
+    res.write("<th>Type</th></tr>");
+    res.write("</table>");
+
+
     
   });
 
