36dc869e6b9eb7b7e26d4bc106e9f8467697e813
WieteskaKarolina <wieteskakarolina02@gmail.com>
2023-04-03 14:51:26 +0200

    add break line

--- lab3/source/server.js
+++ lab3/source/server.js
@@ -21,7 +21,7 @@ app.get('/', function(req, res) {
 
 
     if (!videoFile && !audioFile && !posterImage) {
-        res.send("Please provide a video or audio file.");
+        res.write("Please provide a video or audio file.");
     } 
     
     if(videoFile){
@@ -29,6 +29,7 @@ app.get('/', function(req, res) {
         res.write("source src='" + videoFile +"' />");
         res.write("</video>");
         //button
+        res.write("<br/>");
         res.write("<br/>");
         res.write("<script>function foo1(){ document.getElementById('videoPlayer').src = 'cancel.mp4';}</script>");
         res.write("<button id='videoCancel' onclick='foo1()'>Click me</button>");
@@ -40,6 +41,7 @@ app.get('/', function(req, res) {
         res.write("</audio>");
         //button
         res.write("<br/>");
+        res.write("<br/>");
         res.write("<script>function foo(){ document.getElementById('audioPlayer').src = 'cancel.mp3';}</script>");
         res.write("<button id='audioCancel' onclick='foo()'>Click me</button>");
     }
@@ -47,6 +49,9 @@ app.get('/', function(req, res) {
         res.write("<img src='" + posterImage +"' id='posterImage'>");
     }
 
+    res.write("<br/>");
+    res.write("<br/>");
+    
     res.write("<style>");
     res.write("table, th, td {");
     res.write("  border:1px solid black;}");
